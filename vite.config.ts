f014dca51b78eb862fb73858a8324015648607f4
PutriGultom <putrigultom917@gmail.com>
2025-07-15 11:28:54 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/putriigultom11-reactportfolio/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
